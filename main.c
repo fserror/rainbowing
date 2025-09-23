118b715db2973356d670e1d53e9067c7533f9886
fserror <speakerart229@outlook.com>
2025-09-23 07:56:30 +0000

    added the ability to rainbowize text from a file and check for execution from root

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #define RED     "\x1b[38;5;196m"
 #define ORANGE  "\x1b[38;5;208m"
@@ -10,7 +12,12 @@
 #define VIOLET  "\x1b[38;5;93m"
 #define RESET   "\x1b[0m"
 
-int main() {
+int is_root() {
+    return (getuid() == 0);
+}
+
+void rainbow_io()
+{
     char text[1000];
     const char* colors[] = {RED, ORANGE, YELLOW, GREEN, INDIGO, BLUE, VIOLET};
     int i;
@@ -26,6 +33,69 @@ int main() {
         }
         printf("\n");
     }
+}
+
+void rainbow_file() {
+    const char* colors[] = {RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET};
+    int num_colors = 7;
     
+    char filename[256];
+    printf("Enter absolute path to file: ");
+    
+    if (fgets(filename, sizeof(filename), stdin) == NULL) {
+        perror("Error reading filename");
+        return;
+    }
+    
+    filename[strcspn(filename, "\n")] = '\0';
+    
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL) {
+        perror("Error opening file");
+        return;
+    }
+    
+    char ch;
+    int char_count = 0;
+    
+    while ((ch = fgetc(fp)) != EOF) {
+        int color_index = char_count % num_colors;
+        printf("%s%c%s", colors[color_index], ch, RESET);
+        char_count++;
+    }
+    
+    fclose(fp);
+    printf("\n");
+}
+
+int main() {
+    if (is_root())
+    {
+        printf(RED "WARNING: Program is running by root.\n" RESET);
+    }
+
+    char format[10];
+    
+    printf("\nThis program prints rainbow text :)\n\n");
+    printf("Enter 't' for rainbow text mode, enter 'f' for rainbow text from file, 'q' for quit program: ");
+    
+    if (fgets(format, sizeof(format), stdin) != NULL) {
+        size_t len = strlen(format);
+        if (len > 0 && format[len - 1] == '\n') {
+            format[len - 1] = '\0';
+        }
+        
+        if (strcmp(format, "t") == 0) {
+            rainbow_io();
+        } else if (strcmp(format, "f") == 0) {
+            rainbow_file();
+        } else if (strcmp(format, "q") == 0) {
+            printf("Exiting...");
+            exit(0);
+        } else {
+            printf("Unknown command - %s", format);
+        }
+    }
+
     return 0;
 }
